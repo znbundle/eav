a68421858c9eec6a633e93179efd5d8af79e9f6b
Vitaliy <theyamshikov@yandex.ru>
2021-06-25 15:23:11 +0600

    upd

--- src/Domain/Libs/Rules.php
+++ src/Domain/Libs/Rules.php
@@ -3,6 +3,7 @@
 namespace ZnBundle\Eav\Domain\Libs;
 
 use Illuminate\Support\Collection;
+use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints as Assert;
 use ZnBundle\Eav\Domain\Entities\AttributeEntity;
 use ZnCore\Domain\Helpers\EntityHelper;
@@ -10,46 +11,74 @@ use ZnCore\Domain\Helpers\EntityHelper;
 class Rules
 {
 
+    private $rules = [];
+
     public function convert(Collection $attributesCollection): array
     {
-        $rules = [];
+        $this->rules = [];
         /** @var AttributeEntity $attributeEntity */
         foreach ($attributesCollection as $attributeEntity) {
             $attributeName = $attributeEntity->getName();
-            foreach ($attributeEntity->getRules() as $ruleEntity) {
-                $ruleName = $ruleEntity->getName();
-                $isClassName = strpos($ruleName, '\\') !== false;
-                $ruleClassName = $isClassName ? $ruleName : 'Symfony\Component\Validator\Constraints\\' . ucfirst($ruleName);
-                if ($ruleClassName) {
-                    $params = $ruleEntity->getParams();
-                    if ($params) {
-                        $params = json_decode($params);
-                        $rules[$attributeName][] = new $ruleClassName($params);
-                    } else {
-                        $rules[$attributeName][] = new $ruleClassName();
-                    }
-                }
-            }
-            $enumCollection = $attributeEntity->getEnums();
-            if ($enumCollection && $enumCollection->count() > 0) {
-                $rules[$attributeName][] = new Assert\Choice([
-                    'choices' => EntityHelper::getColumn($enumCollection, 'name'),
-                ]);
-            }
-            $isBoolean = $attributeEntity->getType() == 'boolean' || $attributeEntity->getType() == 'bool';
-            if ($isBoolean) {
-                $rules[$attributeName][] = new Assert\Choice([
-                    'choices' => [true, false],
-                ]);
-            } elseif ($attributeEntity->getIsRequired()) {
-                $rules[$attributeName][] = new Assert\NotBlank;
-            } elseif (!$attributeEntity->getIsRequired()) {
-                $rules[$attributeName][] = new Assert\Length(['min' => 0]);
-            }
-            /*if ($attributeEntity->getIsRequired() && !$isBoolean) {
-                $rules[$attributeName][] = new Assert\NotBlank;
-            }*/
+            $this->handleRules($attributeEntity, $attributeName);
+            $this->castChoices($attributeEntity, $attributeName);
+            $this->castBoolean($attributeEntity, $attributeName);
+            $this->castRequired($attributeEntity, $attributeName);
         }
-        return $rules;
+        return $this->rules;
+    }
+
+    private function addRule(string $attributeName, Constraint $constraint)
+    {
+        $this->rules[$attributeName][] = $constraint;
+    }
+
+    private function handleRules(AttributeEntity $attributeEntity, string $attributeName)
+    {
+        foreach ($attributeEntity->getRules() as $ruleEntity) {
+            $ruleName = $ruleEntity->getName();
+            $isClassName = strpos($ruleName, '\\') !== false;
+            $ruleClassName = $isClassName ? $ruleName : 'Symfony\Component\Validator\Constraints\\' . ucfirst($ruleName);
+            if ($ruleClassName) {
+                $params = $ruleEntity->getParams();
+                if ($params) {
+                    $params = json_decode($params);
+                    $constraintInstance = new $ruleClassName($params);
+                } else {
+                    $constraintInstance = new $ruleClassName();
+                }
+                $this->addRule($attributeName, $constraintInstance);
+            }
+        }
+    }
+
+    private function castChoices(AttributeEntity $attributeEntity, string $attributeName)
+    {
+        $enumCollection = $attributeEntity->getEnums();
+        if ($enumCollection && $enumCollection->count() > 0) {
+            $choiceConstraint = new Assert\Choice([
+                'choices' => EntityHelper::getColumn($enumCollection, 'name'),
+            ]);
+            $this->addRule($attributeName, $choiceConstraint);
+        }
+    }
+
+    private function castBoolean(AttributeEntity $attributeEntity, string $attributeName)
+    {
+        $isBoolean = $attributeEntity->getType() == 'boolean' || $attributeEntity->getType() == 'bool';
+        if ($isBoolean) {
+            $choiceConstraint = new Assert\Choice([
+                'choices' => [true, false],
+            ]);
+            $this->addRule($attributeName, $choiceConstraint);
+        }
+    }
+
+    private function castRequired(AttributeEntity $attributeEntity, string $attributeName)
+    {
+        if ($attributeEntity->getIsRequired()) {
+            $this->addRule($attributeName, new Assert\NotBlank);
+        } elseif (!$attributeEntity->getIsRequired()) {
+            $this->addRule($attributeName, new Assert\Length(['min' => 0]));
+        }
     }
 }
