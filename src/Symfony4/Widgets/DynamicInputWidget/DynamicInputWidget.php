6cc7c0f4a4651d6d8a7ab139362ed7e80f9dbcff
Vitaliy <theyamshikov@yandex.ru>
2021-08-06 11:52:33 +0600

    upd

---
+++ src/Symfony4/Widgets/DynamicInputWidget/DynamicInputWidget.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ZnBundle\Eav\Symfony4\Widgets\DynamicInputWidget;
+
+use Symfony\Component\Form\FormView;
+use ZnLib\Web\Symfony4\MicroApp\Libs\FormRender;
+use ZnLib\Web\Widgets\Base\BaseWidget2;
+
+class DynamicInputWidget extends BaseWidget2
+{
+
+    /** @var FormRender */
+    private $formRender;
+
+    public function setFormRender(FormRender $formRender): void
+    {
+        $this->formRender = $formRender;
+    }
+
+    public function run(): string
+    {
+        $formView = $this->formRender->getFormView();
+        $html = '';
+        foreach ($formView->children as $name => $type) {
+            if ($name != 'save') {
+                $html .= $this->formRender->row($name);
+            }
+        }
+        return $html;
+    }
+}
