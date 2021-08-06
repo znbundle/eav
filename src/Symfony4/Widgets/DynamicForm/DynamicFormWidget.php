1ebef8986d1f8633503a3941c9a5abb86d1bcb50
Vitaliy <theyamshikov@yandex.ru>
2021-08-06 12:57:20 +0600

    upd

---
+++ src/Symfony4/Widgets/DynamicForm/DynamicFormWidget.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ZnBundle\Eav\Symfony4\Widgets\DynamicForm;
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use ZnLib\Web\Symfony4\MicroApp\Libs\FormRender;
+use ZnLib\Web\Widgets\Base\BaseWidget2;
+
+class DynamicFormWidget extends BaseWidget2
+{
+
+    /** @var FormView */
+    private $formView;
+
+    /** @var CsrfTokenManagerInterface */
+    private $tokenManager;
+
+    /** @var FormRender */
+    private $formRender;
+
+    public function __construct(CsrfTokenManagerInterface $tokenManager)
+    {
+        $this->tokenManager = $tokenManager;
+    }
+
+    public function setFormView(FormView $formView): void
+    {
+        $this->formView = $formView;
+    }
+
+    public function setFormRender(FormRender $formRender): void
+    {
+        $this->formRender = $formRender;
+    }
+
+    public function run(): string
+    {
+        if (isset($this->formRender)) {
+            $formRender = $this->formRender;
+        } else {
+            $formRender = new FormRender($this->formView, $this->tokenManager);
+        }
+        return $this->render('index', [
+            'formRender' => $formRender,
+        ]);
+    }
+}
---
+++ src/Symfony4/Widgets/DynamicForm/views/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @var View $view
+ * @var $formRender FormRender
+ */
+
+use ZnBundle\Eav\Symfony4\Widgets\DynamicInput\DynamicInputWidget;
+use ZnCore\Base\Libs\I18Next\Facades\I18Next;
+use ZnLib\Web\Symfony4\MicroApp\Libs\FormRender;
+use ZnLib\Web\View\View;
+
+?>
+
+<?= $formRender->errors() ?>
+
+<?= $formRender->beginFrom() ?>
+
+<?= DynamicInputWidget::widget([
+    'formRender' => $formRender,
+]) ?>
+
+<div class="form-group">
+    <?= $formRender->input('save', 'submit') ?>
+</div>
+
+<?= $formRender->endFrom() ?>
+
+<a href="/restore-password"><?= I18Next::t('user', 'restore-password.request_action') ?></a>
