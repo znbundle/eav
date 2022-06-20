634e928ed6625ce6f887684cbda7951871f06b48
Vitaliy <theyamshikov@yandex.ru>
2022-06-20 14:55:43 +0600

    upd

---
+++ src/Domain/Helpers/EavEntityValidationHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZnBundle\Eav\Domain\Helpers;
+
+use ZnBundle\Eav\Domain\Entities\EntityEntity;
+use ZnBundle\Eav\Domain\Libs\TypeNormalizer;
+use ZnBundle\Eav\Domain\Libs\Validator;
+use ZnCore\Base\Libs\DynamicEntity\Interfaces\ValidateDynamicEntityInterface;
+
+class EavEntityValidationHelper
+{
+
+    public static function validate(ValidateDynamicEntityInterface $dynamicEntity, EntityEntity $entityEntity, array $data): void
+    {
+        $normalizer = new TypeNormalizer();
+        $data = $normalizer->normalizeData($data, $entityEntity);
+        EntityHelper::setAttributes($dynamicEntity, $data);
+        $validator = new Validator();
+        $validator->validate($data, $dynamicEntity->validationRules());
+    }
+}
