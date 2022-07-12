844c6cebdd6056fed39946d5e02b8a4bfdf513c8
Vitaliy <theyamshikov@yandex.ru>
2022-07-12 16:16:21 +0600

    upd

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,4 @@
 
 \ZnCore\DotEnv\Domain\Libs\DotEnv::init();
 
-$_ENV['ELOQUENT_CONFIG_FILE'] = __DIR__ . '/config/eloquent.php';
+//$_ENV['ELOQUENT_CONFIG_FILE'] = __DIR__ . '/config/eloquent.php';
